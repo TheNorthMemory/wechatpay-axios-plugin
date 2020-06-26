1511a554a19934e4afed8e4467fdba4138fa5cab
James ZHANG <thenorthmemory@qq.com>
2020-06-26 20:47:14 +0800

    [optim] params' typing

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const interceptor = require('./lib/interceptor')
 module.exports = interceptor
+module.exports.Rsa = require('./lib/rsa')
+module.exports.Aes = require('./lib/aes')
+module.exports.Formatter = require('./lib/formatter')
 module.exports.default = interceptor
