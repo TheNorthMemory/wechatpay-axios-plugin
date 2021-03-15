acaba894e0deea616e7331d85a8690b43457337e
TheNorthMemory <thenorthmemory@qq.com>
2021-03-15 16:44:45 +0800

    [WIP] transformer.test.js

--- tests/lib/transformer.test.js
+++ tests/lib/transformer.test.js
@@ -6,11 +6,39 @@ describe('lib/transformer', () => {
     Transformer.should.be.a.Function().and.have.property('name', 'Transformer')
   })
 
+  describe('Transformer::mchid', () => {
+    it('method `mchid` should be static and descriped as `get`, `set`, `enumerable` and `configurable` properties', () => {
+      should(Transformer.mchid).be.Undefined()
+      should((new Transformer).mchid).is.Undefined()
+      const describe = Object.getOwnPropertyDescriptor(Transformer, 'mchid')
+      should(describe).have.properties('get', 'set', 'enumerable', 'configurable')
+      should(describe.get).be.a.Function()
+      should(describe.set).be.a.Function()
+    })
+
+    it('method `mchid` should be static usage after called `set("1900000109")` descriptor', () => {
+      Transformer.mchid = '1900000109'
+      should(Transformer.mchid).be.String().and.equal('1900000109')
+      Transformer.mchid = undefined
+      should(Transformer.mchid).be.Undefined()
+    })
+  })
+
   describe('Transformer::secret', () => {
-    Transformer.secret = 'test'
-    it('method `secret` should be static', () => {
-      should(Transformer.secret).be.String().and.equal('test')
+    it('method `secret` should be static and descriped as `get`, `set`, `enumerable` and `configurable` properties', () => {
+      should(Transformer.secret).be.Undefined()
       should((new Transformer).secret).is.Undefined()
+      const describe = Object.getOwnPropertyDescriptor(Transformer, 'secret')
+      should(describe).have.properties('get', 'set', 'enumerable', 'configurable')
+      should(describe.get).be.a.Function()
+      should(describe.set).be.a.Function()
+    })
+
+    it('method `secret` should be static usage after called `set("test")` descriptor', () => {
+      Transformer.secret = 'test'
+      should(Transformer.secret).be.String().and.equal('test')
+      Transformer.secret = undefined
+      should(Transformer.secret).be.Undefined()
     })
   })
 
