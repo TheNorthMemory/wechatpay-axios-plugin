382ec6a1a940528a936ee246226c6e45a1d994a6
TheNorthMemory <thenorthmemory@qq.com>
2021-03-16 14:41:59 +0800

    test cases of the 6833a43 feature

--- tests/lib/transformer.test.js
+++ tests/lib/transformer.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const should = require('should')
 const Transformer = require('../../lib/transformer')
 
@@ -95,12 +96,53 @@ describe('lib/transformer', () => {
       should(Transformer.toXml).be.a.Function()
       should((new Transformer).toXml).is.Undefined()
     })
+
+    it('method `toXml` should throw a `TypeError` while none argument passed in', () => {
+      should(() => {
+        Transformer.toXml()
+      }).throw(TypeError)
+    })
+
+    it('method `toXml` should throw a `TypeError` while a `Symbol` passed in', () => {
+      should(() => {
+        Transformer.toXml(
+          /*eslint-disable-next-line*/
+          Symbol('test')
+        )
+      }).throw(TypeError)
+    })
+
+    it('method `toXml` should returns a string and equal to `<xml/>` while empty string passed in', () => {
+      Transformer.toXml('').should.be.String().and.equal('<xml/>')
+    })
+
+    it('method `toXml` should returns a string and equal to `<xml>test</xml>` while a `test` string passed in', () => {
+      Transformer.toXml('test').should.be.String().and.equal('<xml>test</xml>')
+    })
+
+    it('method `toXml` should returns a string and equal to `<xml><![CDATA[2>1<0]]></xml>` while a `2>1<0` string passed in', () => {
+      Transformer.toXml('2>1<0').should.be.String().and.equal('<xml><![CDATA[2>1<0]]></xml>')
+    })
+
+    it('method `toXml` should returns a string and equal to `<xml/>` while an empty object passed in', () => {
+      Transformer.toXml({}).should.be.String().and.equal('<xml/>')
+    })
+
+    it('method `toXml` should returns a string and equal to `<xml><mch_id>1900000109</mch_id><sign>mock</sign></xml>` while the `{mch_id: \'1900000109\', sign: \'mock\'}` object passed in', () => {
+      Transformer.toXml({mch_id: '1900000109', sign: 'mock'}).should.be.String().and.equal('<xml><mch_id>1900000109</mch_id><sign>mock</sign></xml>')
+    })
   })
 
   describe('Transformer::request', () => {
     it('method `request` should be static', () => {
       should(Transformer.request).be.Array()
       should((new Transformer).request).is.Undefined()
+    })
+
+    it('method `request` should be equal to `[Transformer.signer, Transformer.toXml]`', () => {
+      Transformer.request.should.be.Array().and.length(2)
+      Transformer.request[0].should.be.equal(Transformer.signer)
+      Transformer.request[1].should.be.equal(Transformer.toXml)
     })
   })
 
@@ -109,6 +151,33 @@ describe('lib/transformer', () => {
       should(Transformer.toObject).be.a.Function()
       should((new Transformer).toObject).is.Undefined()
     })
+
+    it('method `toObject` should returns `undefined` while none argument passed in', () => {
+      should(Transformer.toObject()).be.Undefined()
+    })
+
+    it('method `toObject` should returns `undefined` while a `Symbol` passed in', () => {
+      should(Transformer.toObject(
+        /*eslint-disable-next-line*/
+        Symbol('test')
+      )).be.Undefined()
+    })
+
+    it('method `toObject` should returns `undefined` while a `test` string passed in', () => {
+      should(Transformer.toObject('test')).be.Undefined()
+    })
+
+    it('method `toObject` should returns `test` while a `<span>test</span>` string passed in', () => {
+      should(Transformer.toObject('<span>test</span>')).be.equal('test')
+    })
+
+    it('method `toObject` should returns `{mch_id: \'10000100\'}` while a `<xml><mch_id>10000100</mch_id></xml>` string passed in', () => {
+      should(Transformer.toObject(`<xml><mch_id>10000100</mch_id></xml>`)).be.eql({mch_id: '10000100'})
+    })
+
+    it('method `toObject` should returns `Buffer` while a `Buffer.from([])` passed in', () => {
+      should(Transformer.toObject(Buffer.from([]))).be.eql(Buffer.from([]))
+    })
   })
 
   describe('Transformer::verifier', () => {
@@ -116,12 +185,36 @@ describe('lib/transformer', () => {
       should(Transformer.verifier).be.a.Function()
       should((new Transformer).verifier).is.Undefined()
     })
+
+    it('method `verifier` should throw `assert.AssertionError` while the input `{sign}` doesn\'t matched', () => {
+      should(() => {
+        Transformer.verifier({sign: '332F17B766FC787203EBE9D6E40457A1'})
+      }).throw(assert.AssertionError)
+    })
+
+    it('method `verifier` should returns `test` while a `test` string passed in', () => {
+      should(Transformer.verifier('test')).be.eql('test')
+    })
+
+    it('method `verifier` should returns `{}` while a `{}` object passed in', () => {
+      should(Transformer.verifier({})).be.eql({})
+    })
+
+    it('method `verifier` should returns `Buffer` while a `Buffer.from([])` passed in', () => {
+      should(Transformer.verifier(Buffer.from([]))).be.eql(Buffer.from([]))
+    })
   })
 
   describe('Transformer::response', () => {
     it('method `response` should be static', () => {
       should(Transformer.response).be.Array()
       should((new Transformer).response).is.Undefined()
+    })
+
+    it('method `response` should be equal to `[Transformer.toObject, Transformer.verifier]`', () => {
+      Transformer.response.should.be.Array().and.length(2)
+      Transformer.response[0].should.be.equal(Transformer.toObject)
+      Transformer.response[1].should.be.equal(Transformer.verifier)
     })
   })
 })
