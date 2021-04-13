70df43f944156e8f3d1d867e55356583dfe8f50a
TheNorthMemory <thenorthmemory@qq.com>
2021-04-13 21:38:12 +0800

    chore(Feature): better performance by the ES6 `Reflect.set`

--- bin/cli.js
+++ bin/cli.js
@@ -12,33 +12,28 @@ yargs(hideBin(process.argv))
   .wrap(null)
   .parserConfiguration({ 'camel-case-expansion': false })
   .option('baseURL', {
-    type: 'string',
     alias: 'u',
+    type: 'string',
     default: 'https://api.mch.weixin.qq.com/',
     describe: 'The baseURL',
-    normalize: false,
   })
   .epilog('for more information visit https://github.com/TheNorthMemory/wechatpay-axios-plugin')
   .middleware((argv) => {
     if (argv.c && argv.c.privateKey && argv.c.privateKey !== 'any') {
-      /* eslint-disable-next-line no-param-reassign */
-      argv.config.privateKey = readFileSync(argv.c.privateKey);
+      Reflect.set(argv.config, 'privateKey', readFileSync(argv.c.privateKey));
     }
     if (argv.c && argv.c.certs && Object.keys(argv.c.certs)[0] !== 'any') {
       Object.entries(argv.c.certs).reduce((o, [k, v]) => Object.assign(o, { [k]: readFileSync(v) }), argv.config.certs);
     }
     if (argv.c && argv.c.merchant) {
       if (argv.c.merchant.cert && argv.c.merchant.cert !== 'any') {
-        /* eslint-disable-next-line no-param-reassign */
-        argv.config.merchant.cert = readFileSync(argv.c.merchant.cert);
+        Reflect.set(argv.config.merchant, 'cert', readFileSync(argv.c.merchant.cert));
       }
       if (argv.c.merchant.key && argv.c.merchant.key !== 'any') {
-        /* eslint-disable-next-line no-param-reassign */
-        argv.config.merchant.key = readFileSync(argv.c.merchant.key);
+        Reflect.set(argv.config.merchant, 'key', readFileSync(argv.c.merchant.key));
       }
       if (argv.c.merchant.pfx && argv.c.merchant.pfx !== 'any') {
-        /* eslint-disable-next-line no-param-reassign */
-        argv.config.merchant.pfx = readFileSync(argv.c.merchant.pfx);
+        Reflect.set(argv.config.merchant, 'pfx', readFileSync(argv.c.merchant.pfx));
       }
     }
   }, true)
