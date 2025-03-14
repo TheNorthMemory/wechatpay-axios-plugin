fac4c9671085cac69a9a354ac0784f7f16387def
TheNorthMemory <thenorthmemory@qq.com>
2025-03-14 21:48:02 +0800

    chore: `v2.papay.h5entrustweb.get.test.js`

---
+++ tests/openapi/v2.papay.h5entrustweb.get.test.js
@@ -0,0 +1,46 @@
+require('should');
+const nock = require('nock');
+
+const { Wechatpay } = require('../..');
+
+describe('v2/papay/h5entrustweb', () => {
+  let scope;
+  let instance;
+
+  const mocks = (requestUri) => {
+    requestUri.should.be.String().and.match(/^\/papay\/h5entrustweb\?mch_id=101&nonce_str=[^=]{32}&sign=[\d\w]{32}$/);
+    return [
+      '<xml>',
+      '<return_code>SUCCESS</return_code>',
+      '<result_code>SUCCESS</result_code>',
+      '<redirect_url>http://weixin.qq.com/</redirect_url>',
+      '</xml>',
+    ].join('');
+  };
+
+  beforeEach(() => {
+    instance = new Wechatpay({
+      mchid: '101',
+      serial: 'nop',
+      privateKey: '',
+      certs: { any: '' },
+    });
+
+    scope = nock('https://api.mch.weixin.qq.com')
+      .defaultReplyHeaders({
+        server: 'Nginx',
+        'Content-Type': 'text/xml',
+      })
+      .replyDate();
+  });
+
+  describe('GET `v2/papay/h5entrustweb`', () => {
+    it('GET the `v2/papay/h5entrustweb` should be a `200` response', async () => {
+      scope.get((uri) => uri.startsWith('/papay/h5entrustweb')).reply(200, mocks);
+      await instance['v2/papay/h5entrustweb'].get({ params: { mch_id: '101' } }).then((res) => {
+        res.should.be.an.Object().and.have.keys('headers', 'data');
+        res.data.should.be.an.Object().and.have.keys('return_code', 'result_code', 'redirect_url');
+      });
+    });
+  });
+});
